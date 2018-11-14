1fa158e478a902c65ae5ca03c96db4bb68765d0f
davieu <davisumana@gmail.com>
2018-11-14 18:00:50 -0500

    added scroll event for navbar to add shadow & polished some code and removed exccess classes

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+var last_known_scroll_position = 0;
+var ticking = false;
+
 let domStrings = {
+    navbarDOM: ".navbar",  
     navbarBrandDOM: ".navbar-brand",
+    navbarShadowDOM: "navbar-shadow",
     bracketNavbarBrandDOM: "bracket-navbar-brand"
 }
 
+
+/****************************************************************************************
+ * Hover function to make the brackets for the navbar-brand change color
+ ****************************************************************************************/
 document.querySelector(domStrings.navbarBrandDOM).addEventListener("mouseover", mouseOver);
 
 document.querySelector(domStrings.navbarBrandDOM).addEventListener("mouseout", mouseOut);
@@ -20,3 +29,37 @@ function mouseOut() {
         brackets[i].style.color = "black";
     }
 }
+
+
+/****************************************************************************************
+ * scroll function so that when the navbar Y position is greater than 30 then navbar-shadow class is added
+ ****************************************************************************************/
+
+let element = document.querySelector(domStrings.navbarDOM)
+
+function doSomething(scroll_pos) {
+    // do something with the scroll position
+    if (scroll_pos > 30) {
+        element.classList.add(domStrings.navbarShadowDOM)
+      } else {
+          element.classList.remove(domStrings.navbarShadowDOM)
+      }
+  }
+  
+  window.addEventListener('scroll', function(e) {
+  
+    last_known_scroll_position = window.scrollY;
+    //scrolling magic to lessen the pressure calls of the scroll event
+    if (!ticking) {
+  
+      window.requestAnimationFrame(function() {
+        doSomething(last_known_scroll_position);
+        ticking = false;
+      });
+      
+      ticking = true;
+    }
+
+  });
+
+
